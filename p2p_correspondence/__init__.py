e819f5f23f394712b544a4377f938e7009a33763
shahar.zuler <shaharzuler@gmail.com>
2023-10-24 17:21:34 +0300

    adding get default config func

--- p2p_correspondence/__init__.py
+++ p2p_correspondence/__init__.py
@@ -1,5 +1,2 @@
 from .src.main import get_correspondence
-
-# from .src.utils.fm_utils import * 
-# from .src.utils.os_utils import * 
-# from .src.utils.visualization_utils import * 
+from .src.utils.os_utils import get_default_config
--- p2p_correspondence/src/utils/os_utils.py
+++ p2p_correspondence/src/utils/os_utils.py
@@ -1,4 +1,19 @@
 import os
+import json
+from typing import Dict
+import pkg_resources
+
+def _get_config(json_path:str) -> Dict:
+    package_name = __name__.split('.')[0]
+    file_path = pkg_resources.resource_filename(package_name, json_path)
+    with open(file_path) as file:
+        args = json.load(file)
+    return args
+
+def get_default_config() -> Dict:
+    json_path = os.path.join('src','default_config.json')
+    args = _get_config(json_path)
+    return args
 
 def create_output_dir(config):
     exp_dir = f'_n_ev_{config.process_params.n_ev}_n_descr_{config.process_params.n_descr}_descr_type_{config.process_params.descr_type}_nit_{config.zoomout_refine_params.nit}_step_{config.zoomout_refine_params.step}'
