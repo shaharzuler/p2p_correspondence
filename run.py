8278ef6ce6d85c3946c29689ba51983be534deeb
shahar.zuler <shaharzuler@gmail.com>
2023-10-24 17:30:45 +0300

    fixing run file for proper package use

--- run.py
+++ run.py
@@ -1,11 +1,10 @@
-from p2p_correspondence.src.main import get_correspondence
 from easydict import EasyDict
-import json
+
+import p2p_correspondence
 
 mesh1_path = "/home/shahar/cardio_corr/outputs/magix/synthetic_dataset100/01/orig/meshes/smooth_mesh.off"
 mesh2_path = "/home/shahar/cardio_corr/outputs/magix/synthetic_dataset100/28/orig/meshes/smooth_mesh.off"
-with open("/home/shahar/cardio_corr/my_packages/p2p_correspondence_project/p2p_correspondence/p2p_correspondence/src/default_config.json") as f:
-    config = EasyDict(json.load(f))
+config = p2p_correspondence.get_default_config()
 
-inference_path = get_correspondence(mesh1_path, mesh2_path, config)
+inference_path = p2p_correspondence.get_correspondence(mesh1_path, mesh2_path, EasyDict(config))
 print(inference_path)
